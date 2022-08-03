344e5927f6c4a1ee672cc3feda4a08c80346fa7e
AInine <ainine.jp@gmail.com>
2022-08-03 14:39:43 +0900

    /v1/statuses/へのルーティングを設定

---
+++ app/handler/statuses/router.go
@@ -0,0 +1,22 @@
+package statuses
+
+import (
+	"net/http"
+	"yatter-backend-go/app/app"
+
+	"github.com/go-chi/chi"
+)
+
+// Implementation of handler
+type handler struct {
+	app *app.App
+}
+
+// Create Handler for `/v1/statuses/`
+func NewRouter(app *app.App) http.Handler {
+	r := chi.NewRouter()
+
+	h := &handler{app: app}
+
+	return r
+}
